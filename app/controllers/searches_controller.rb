6119cffcd703c919e04002dbf185891014e108e5
Brittany Morris <brittmmendez@gmail.com>
2018-04-02 21:53:50 +0000

    automatically backed up by learn

--- app/controllers/searches_controller.rb
+++ app/controllers/searches_controller.rb
@@ -20,7 +20,7 @@ class SearchesController < ApplicationController
       end
     rescue Faraday::ConnectionFailed
     @error = "There was a timeout. Please try again."
-    end
     render 'search'
+    end
     
 end
